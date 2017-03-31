c41fea73ec189bf8bc44ac5497b980633e2b225a
amlak befekadu <amlakgirma@yahoo.com>
2017-03-31 09:04:49 -0400

    final atm project

--- atm.js
+++ atm.js
@@ -1,32 +1,47 @@
 
 
 $(document).ready(function(){
-//==========Checking Account=========================
-  $("#checking .deposit").on("click", function() {
 
-      var ckBalance = parseInt($("#checking .balance").text().replace("$", ""));
-      var ckDeposit = parseInt($("#checking .input").val());
+  ///Declare the variable
+var balance = $("#checking .balance");
+var input = $("#checking .input");
+var deposit = $("#checking .deposit");
+//==========Checking Account=========================
+      (deposit).on("click", function() {
+
+      var ckBalance = parseInt((balance).text().replace("$", ""));
+      var ckDeposit = parseInt((input).val());
       var ckTotal = ckBalance + ckDeposit;
-      $("#checking .balance").text("$" + ckTotal);
+      (balance).text("$" + ckTotal);
     });
 //==================== checking withdraw=====================
+      var checkbalance = $("#checking .balance");
+      var checkInput = $("#checking .input");
+      var checkWithDraw = $("#checking .withdraw");
 
-    $("#checking .withdraw").on("click", function() {
-      var ckBalance = parseInt($("#checking .balance").text().replace("$", ""));
-      var ckWithdraw = parseInt($("#checking .input").val());
+    (checkWithDraw).on("click", function() {
+      var ckBalance = parseInt($(checkbalance).text().replace("$", ""));
+      var ckWithdraw = parseInt((checkInput).val());
       var ckTotal = ckBalance - ckWithdraw;
-      $("#checking .balance").text("$" + ckTotal);
+      (checkbalance).text("$" + ckTotal);
     });
 
 //============== Saving Deposit===========================
-    $("#savings .deposit").on("click", function() {
-      var cvBalance = parseInt($("#savings .balance").text().replace("$", ""));
-      var cvDeposit = parseInt($("#savings .input").val());
+
+      var saveDeposit = $("#savings .deposit");
+      var savebalance = $("#savings .balance");
+      var saveInput = $("#savings .input");
+
+            (saveDeposit).on("click", function() {
+      var cvBalance = parseInt((savebalance).text().replace("$", ""));
+      var cvDeposit = parseInt((saveInput).val());
       var cvTotal = cvBalance + cvDeposit;
-      $("#savings .balance").text("$" + cvTotal);
+       (savebalance).text("$" + cvTotal);
     });
 //============Saving withdraw===================
-    $("#savings .withdraw").on("click", function() {
+      var saveWithdraw = $("#savings .withdraw");
+      
+        $(saveWithdraw).on("click", function() {
       var cvBalance = parseInt($("#savings .balance").text().replace("$", ""));
       var cvWithdraw = parseInt($("#savings .input").val());
       var cvTotal = cvBalance - cvWithdraw;
